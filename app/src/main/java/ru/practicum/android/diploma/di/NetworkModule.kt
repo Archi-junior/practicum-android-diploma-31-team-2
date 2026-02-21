b4ed0a96b3b7db558f327ed0dd284d9132028cdd
qu1eres <qu1eres@yandex.ru>
2026-02-22 01:54:39 +0300

    base retrofit & gson di

--- app/src/main/java/ru/practicum/android/diploma/di/NetworkModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/NetworkModule.kt
@@ -1,9 +1,17 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val networkModule = module {
+    single { createRetrofit() }
 
 }
 
-
+private fun createRetrofit(): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl("https://practicum-diploma-8bc38133faba.herokuapp.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
