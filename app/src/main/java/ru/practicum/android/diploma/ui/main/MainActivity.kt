661486380df44c6d6e41f716d7b60a89905fd36c
qu1eres <qu1eres@yandex.ru>
2026-02-22 03:45:05 +0300

    activity's

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/main/MainActivity.kt
@@ -0,0 +1,29 @@
+package ru.practicum.android.diploma.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    private fun setupNavigation() {
+
+    }
+
+}
