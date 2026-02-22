35c8846b85661d3b4b100bc767011d972f98c837
qu1eres <qu1eres@yandex.ru>
2026-02-22 05:28:30 +0300

    detekt problem fix 2

--- app/src/main/java/ru/practicum/android/diploma/ui/branch_choose/BranchChooseFragment.kt
+++
@@ -1,6 +0,0 @@
-package ru.practicum.android.diploma.ui.branch_choose
-
-import androidx.fragment.app.Fragment
-import ru.practicum.android.diploma.R
-
-class BranchChooseFragment : Fragment(R.layout.branch_choose_fragment)
--- app/src/main/java/ru/practicum/android/diploma/ui/work_choose/WorkChooseFragment.kt
+++
@@ -1,6 +0,0 @@
-package ru.practicum.android.diploma.ui.work_choose
-
-import androidx.fragment.app.Fragment
-import ru.practicum.android.diploma.R
-
-class WorkChooseFragment : Fragment(R.layout.work_choose_fragment)
