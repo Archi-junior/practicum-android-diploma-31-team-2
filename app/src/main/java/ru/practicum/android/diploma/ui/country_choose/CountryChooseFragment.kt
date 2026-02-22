5667974359d7cffd932d92867c29f3342a3d7d62
qu1eres <qu1eres@yandex.ru>
2026-02-22 04:15:41 +0300

    all empty fragments part 2

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/country_choose/CountryChooseFragment.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.ui.country_choose
+
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
+
+class CountryChooseFragment : Fragment(R.layout.country_choose_fragment) {
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/region_choose/RegionChooseFragment.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.ui.region_choose
+
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
+
+class RegionChooseFragment : Fragment(R.layout.region_choose_fragment) {
+}
