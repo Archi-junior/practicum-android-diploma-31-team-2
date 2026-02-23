de84f0967ffb2bc1cb52ab408b1af83bde0bafe7
vladimir-vrn <dvv_mail@mail.ru>
2026-02-23 22:07:40 +0300

    Добавил класс Entity,DAO интерфейс, класс AppDatabase 1,5ч

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -4,11 +4,21 @@ import androidx.room.Room
 import com.google.gson.Gson
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.db.AppDatabase
+import ru.practicum.android.diploma.data.db.FavoriteVacanciesDao
 
 val dataModule = module {
     factory { Gson() }
 
-    // Экземпляр датабазы. Раскоментировать как только Вовчик выполнит второй таск
-    single { //Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database.db").build()
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            AppDatabase::class.java,
+            "database.db")
+            .build()
+    }
+
+    factory<FavoriteVacanciesDao> {
+        get<AppDatabase>().favoriteVacanciesDao()
     }
 }
