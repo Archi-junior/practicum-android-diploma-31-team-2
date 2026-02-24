2c85689f662c08bc3ad65e4113eb9f04f493bf83
vladimir-vrn <dvv_mail@mail.ru>
2026-02-24 13:49:59 +0300

    Добавил инициализацию ApiService 0,5ч

--- app/src/main/java/ru/practicum/android/diploma/di/NetworkModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/NetworkModule.kt
@@ -1,20 +1,58 @@
 package ru.practicum.android.diploma.di
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.BuildConfig
+import ru.practicum.android.diploma.data.network.ApiService
+import ru.practicum.android.diploma.data.network.NetworkClient
+import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
 
 val networkModule = module {
-    single { createRetrofit() }
 
-    // Экземпляр ApiService. Раскоментировать как только Вовчик выполнит третий таск
-    single { //get<Retrofit>().create(ApiService::class.java)
+    single<Interceptor>(qualifier = named("auth")) {
+        Interceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("Authorization", "Bearer ${BuildConfig.API_ACCESS_TOKEN}")
+                .build()
+            chain.proceed(request)
+        }
     }
-}
 
-private fun createRetrofit(): Retrofit {
-    return Retrofit.Builder()
-        .baseUrl("https://practicum-diploma-8bc38133faba.herokuapp.com/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    single {
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.HEADERS
+        }
+    }
+
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(get<HttpLoggingInterceptor>())
+            .addInterceptor(get<Interceptor>(named("auth")))
+            .build()
+    }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl("https://practicum-diploma-8bc38133faba.herokuapp.com/")
+            .client(get<OkHttpClient>())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    single<ApiService> {
+        get<Retrofit>().create(ApiService::class.java)
+    }
+
+    single<NetworkClient> {
+        RetrofitNetworkClient(
+            androidContext(),
+            get()
+        )
+    }
 }
