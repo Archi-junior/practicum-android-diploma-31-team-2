150a088a130404d16b1149ad89c0c7194cc4be23
Archi-junior <arman5dosov@mail.ru>
2026-03-03 02:02:31 +0300

    Исправление ошибок detect в SearchFragment

--- app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
@@ -3,11 +3,11 @@ package ru.practicum.android.diploma.ui.search
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.SearchFragmentBinding
@@ -19,7 +19,7 @@ class SearchFragment : Fragment(R.layout.search_fragment) {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = SearchFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -28,17 +28,13 @@ class SearchFragment : Fragment(R.layout.search_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.searchEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-            override fun afterTextChanged(s: Editable?) {
-                val hasText = !s.isNullOrBlank()
-                val endDrawable = if (hasText) R.drawable.ic_clear else R.drawable.ic_search
-                binding.searchEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                    0, 0, endDrawable, 0
-                )
-            }
-        })
+        binding.searchEditText.addTextChangedListener { s: Editable? ->
+            val hasText = !s.isNullOrBlank()
+            val endDrawable = if (hasText) R.drawable.ic_clear else R.drawable.ic_search
+            binding.searchEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                0, 0, endDrawable, 0
+            )
+        }
 
         binding.searchEditText.setOnTouchListener { v, event ->
             if (event.action == MotionEvent.ACTION_UP) {
