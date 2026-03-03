ba53a45d3af03f6a6e9d5be62c21bf4ccee62dec
vladimir-vrn <dvv_mail@mail.ru>
2026-03-03 14:42:26 +0300

    Добавил обработку кнопки Назад 0.25 ч

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.core.text.HtmlCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -38,6 +39,7 @@ class VacancyFragment : Fragment(){
             render(it)
         }
 
+        binding.ivBack.setOnClickListener { findNavController().navigateUp() }
         binding.ivFavorite.setOnClickListener { viewModel.onAddedToFavorites() }
     }
 
