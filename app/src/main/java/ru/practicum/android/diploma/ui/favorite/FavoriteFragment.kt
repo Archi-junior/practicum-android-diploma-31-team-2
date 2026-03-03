9f6c2dc345dc07b78a15095ad19372a54227a1da
vladimir-vrn <dvv_mail@mail.ru>
2026-03-03 21:25:41 +0300

    Сделал макет, исправил фрагмент 1 ч

--- app/src/main/java/ru/practicum/android/diploma/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/favorite/FavoriteFragment.kt
@@ -1,11 +1,30 @@
 package ru.practicum.android.diploma.ui.favorite
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FavoriteFragmentBinding
 
-class FavoriteFragment : Fragment(R.layout.favorite_fragment){
+class FavoriteFragment : Fragment(){
+
+    private var _binding: FavoriteFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FavoriteFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
