1603b0fd737d03a4f7f2c6713bb710c66674dc67
vladimir-vrn <dvv_mail@mail.ru>
2026-03-07 02:00:52 +0300

    Исправил описание FilterSettings 0.25 ч

--- app/src/main/java/ru/practicum/android/diploma/domain/models/FilterSettings.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/FilterSettings.kt
@@ -1,12 +1,9 @@
 package ru.practicum.android.diploma.domain.models
 
 data class FilterSettings(
-    val countryId: String?,
-    val countryName: String?,
-    val regionId: String?,
-    val regionName: String?,
-    val industryId: String?,
-    val industryName: String?,
+    val country: Area?,
+    val region: Area?,
+    val industry: Industry?,
     val salary: Int = 0,
     val onlyWithSalary: Boolean = false,
 )
