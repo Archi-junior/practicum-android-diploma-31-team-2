780cff236b5166dca14e252eda1e91a49e657c3b
vladimir-vrn <dvv_mail@mail.ru>
2026-03-07 16:15:10 +0300

    Удалил заглушки для FilterSettings и SettingsInteractor 0.25 ч

--- app/src/main/java/ru/practicum/android/diploma/domain/models/FakeFilterSettings.kt
+++
@@ -1,9 +0,0 @@
-package ru.practicum.android.diploma.domain.models
-
-data class FakeFilterSettings(
-    val country: Area?,
-    val region: Area?,
-    val industry: Industry?,
-    val salary: Int?,
-    val onlyWithSalary: Boolean = false,
-)
