6602e8cb47fafc885dbe7c7361fbbbbfc1d56109
vladimir-vrn <dvv_mail@mail.ru>
2026-03-08 13:06:23 +0300

    Доработал рендер экрана 1 ч

--- app/src/main/java/ru/practicum/android/diploma/ui/country/CountryChooseFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/country/CountryChooseFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.activityViewModel
@@ -16,7 +17,6 @@ class CountryChooseFragment : Fragment() {
     private val viewModel: SharedViewModel by activityViewModel()
     private var _binding: CountryChooseFragmentBinding? = null
     private val binding get() = _binding!!
-
     private lateinit var adapter: CountryAdapter
 
     override fun onCreateView(
@@ -47,15 +47,38 @@ class CountryChooseFragment : Fragment() {
     private fun render(state: CountryChooseState) {
         when (state) {
             is CountryChooseState.Initial -> Unit
-            is CountryChooseState.NoConnection -> Unit
+            is CountryChooseState.NoConnection -> {
+                binding.apply {
+                    rvCountries.isVisible = false
+                    progressBar.isVisible = false
+                    inclNoInternet.root.isVisible = true
+                    inclServerError.root.isVisible = false
+                }
+            }
             is CountryChooseState.Error -> {
-
+                binding.apply {
+                    rvCountries.isVisible = false
+                    progressBar.isVisible = false
+                    inclNoInternet.root.isVisible = false
+                    inclServerError.root.isVisible = true
+                }
             }
             is CountryChooseState.Loading -> {
-
+                binding.apply {
+                    rvCountries.isVisible = false
+                    progressBar.isVisible = true
+                    inclNoInternet.root.isVisible = false
+                    inclServerError.root.isVisible = false
+                }
             }
             is CountryChooseState.Content -> {
-
+                adapter.submitList(state.areas)
+                binding.apply {
+                    rvCountries.isVisible = true
+                    progressBar.isVisible = false
+                    inclNoInternet.root.isVisible = false
+                    inclServerError.root.isVisible = false
+                }
             }
         }
     }
