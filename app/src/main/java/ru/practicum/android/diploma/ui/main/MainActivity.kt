067d20607f4ca8aec56bed8d5a01ff9fb0fb7962
Sergey <96-94@mail.ru>
2026-03-11 23:08:27 +0300

    Исправление багов

--- app/src/main/java/ru/practicum/android/diploma/ui/main/MainActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma.ui.main
 import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
@@ -23,7 +24,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupNavigation()
@@ -34,31 +34,27 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
         navController = navHostFragment.navController
+        setupBottomNavigation()
+        setupDestinationListener()
+    }
+
+    private fun setupBottomNavigation() {
         binding.bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.searchFragment -> {
-                    navController.navigate(R.id.searchFragment)
-                }
-
-                R.id.favoriteFragment -> {
-                    navController.navigate(R.id.favoriteFragment)
-                }
-
-                R.id.teamFragment -> {
-                    navController.navigate(R.id.teamFragment)
-                }
+                R.id.searchFragment -> navController.navigate(R.id.searchFragment)
+                R.id.favoriteFragment -> navController.navigate(R.id.favoriteFragment)
+                R.id.teamFragment -> navController.navigate(R.id.teamFragment)
             }
             true
         }
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            updateUiForDestination(destination.id)
-        }
     }
 
-    private fun updateUiForDestination(destinationId: Int) {
-        updateTitle(destinationId)
-        updateFilterButtonVisibility(destinationId)
-        updateFilterButtonColor(destinationId)
+    private fun setupDestinationListener() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            updateTitle(destination.id)
+            updateFilterButtonVisibility(destination.id)
+            updateFilterButtonColor(destination.id)
+        }
     }
 
     private fun setupFilterButton() {
@@ -68,77 +64,75 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateTitle(destinationId: Int) {
-        val titleResId = when (destinationId) {
-            R.id.searchFragment -> R.string.job_search
-            R.id.favoriteFragment -> R.string.favorites
-            R.id.teamFragment -> R.string.team
-            R.id.filtersFragment -> R.string.filter_title
-            R.id.branchChooseFragment -> R.string.choosing_an_industry
-            R.id.workChooseFragment -> R.string.choosing_a_place_of_work
-            R.id.countryChooseFragment -> R.string.choosing_country
-            R.id.regionChooseFragment -> R.string.choosing_region
-            else -> {
-                R.string.app_name
-            }
-        }
         when (destinationId) {
             R.id.vacancyFragment -> {
                 binding.bottomNavigationView.isVisible = false
-                hideTitle()
-            }
-
-            R.id.favoriteFragment -> {
-                binding.bottomNavigationView.isVisible = true
-                hideTitle()
-
             }
 
             else -> {
-                showTitle()
                 binding.bottomNavigationView.isVisible = true
             }
         }
-        binding.titleText.setText(titleResId)
-    }
-
-    private fun hideTitle() {
-        binding.titleText.visibility = android.view.View.GONE
-    }
-
-    private fun showTitle() {
-        binding.titleText.visibility = android.view.View.VISIBLE
-    }
-
-    private fun updateFilterButtonVisibility(destinationId: Int) {
-        binding.filterButton.visibility = if (destinationId == R.id.searchFragment) {
-            android.view.View.VISIBLE
-        } else {
-            android.view.View.GONE
+        binding.titleText.apply {
+            text = getTitleText(destinationId)
+            visibility = if (shouldHideTitle(destinationId)) View.GONE else View.VISIBLE
         }
     }
 
-    fun updateFilterButtonColor(destinationId: Int) {
-
-        if (destinationId != R.id.searchFragment) return
-
-        val emptyFilter = FilterSettings(
-            country = null,
-            region = null,
-            industry = null,
-            salary = 0,
-            onlyWithSalary = false,
-        )
-        val filterSettings = settingsInteractor.getFilterSettings()
-        if (filterSettings == null || filterSettings == emptyFilter) {
-            val typedValue = TypedValue()
-            theme.resolveAttribute(
-                com.google.android.material.R.attr.colorOnPrimary,
-                typedValue,
-                true
+        private fun getTitleText(destinationId: Int): String {
+            return getString(
+                when (destinationId) {
+                    R.id.searchFragment -> R.string.job_search
+                    R.id.favoriteFragment -> R.string.favorites
+                    R.id.teamFragment -> R.string.team
+                    R.id.countryChooseFragment -> R.string.choosing_country
+                    R.id.regionChooseFragment -> R.string.choosing_region
+                    else -> R.string.app_name
+                }
             )
-            binding.filterButton.imageTintList = ColorStateList.valueOf(typedValue.data)
-        } else {
-            binding.filterButton.imageTintList = ContextCompat.getColorStateList(this, R.color.blue)
+        }
+
+        private fun shouldHideTitle(destinationId: Int): Boolean {
+            return destinationId in listOf(
+                R.id.vacancyFragment,
+                R.id.favoriteFragment,
+                R.id.filtersFragment,
+                R.id.workChooseFragment,
+                R.id.countryChooseFragment,
+                R.id.regionChooseFragment
+            )
+        }
+
+        private fun updateFilterButtonVisibility(destinationId: Int) {
+            binding.filterButton.visibility = if (destinationId == R.id.searchFragment) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+        }
+
+        private fun updateFilterButtonColor(destinationId: Int) {
+
+            if (destinationId != R.id.searchFragment) return
+
+            val emptyFilter = FilterSettings(
+                country = null,
+                region = null,
+                industry = null,
+                salary = 0,
+                onlyWithSalary = false,
+            )
+            val filterSettings = settingsInteractor.getFilterSettings()
+            if (filterSettings == null || filterSettings == emptyFilter) {
+                val typedValue = TypedValue()
+                theme.resolveAttribute(
+                    com.google.android.material.R.attr.colorOnPrimary,
+                    typedValue,
+                    true
+                )
+                binding.filterButton.imageTintList = ColorStateList.valueOf(typedValue.data)
+            } else {
+                binding.filterButton.imageTintList = ContextCompat.getColorStateList(this, R.color.blue)
+            }
         }
     }
-}
